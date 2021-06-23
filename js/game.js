26e3bb4a12ad90dd39d73a3bbc145fec7200bb03
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-06-23 07:05:49 +0700

    100% screen

--- js/game.js
+++ js/game.js
@@ -182,11 +182,7 @@ class game {
 
     render() {
         this.canvas.width = document.documentElement.clientWidth;
-        this.canvas.height = this.canvas.width * 0.7;
-        if (this.canvas.height > document.documentElement.clientHeight) {
-            this.canvas.height = document.documentElement.clientHeight;
-            this.canvas.width = this.canvas.height / 0.7;
-        }
+        this.canvas.height = document.documentElement.clientHeight;
         game_W = this.canvas.width;
         game_H = this.canvas.height;
     }
